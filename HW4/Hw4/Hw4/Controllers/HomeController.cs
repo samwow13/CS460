d543f33bbe8d0dc7af70dd201e4884b649f7ad0f
Sam Wetzel <samwow13@yahoo.com>
2017-10-02 11:26:38 -0700

    Added GetPage to lower case

--- HW4/Hw4/Hw4/Controllers/HomeController.cs
+++ HW4/Hw4/Hw4/Controllers/HomeController.cs
@@ -13,5 +13,20 @@ namespace Hw4.Controllers
         {
             return View();
         }
+
+        public ActionResult GetPage()
+        {
+            if (Request.QueryString.HasKeys())
+            {
+                string one = Request.QueryString["input1"].Trim().ToLower();
+                string two = Request.QueryString["input2"].Trim().ToLower();
+                ViewBag.caseHeader = "Your input:";
+
+                one = one.Equals("") ? one : one.First().ToString() + one.Substring(1);
+                ViewBag.additionMessage = "Your names are magically lowercase! "  + one + " " + two;
+            }
+            return View();
+        }
+
     }
 }
