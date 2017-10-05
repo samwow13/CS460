942612b3c0065f82d8c6c2b301750204865acb8c
Sam Wetzel <samwow13@yahoo.com>
2017-10-04 19:15:07 -0700

    Added LoanCalc View and Controller Method

--- HW4/Hw4/Hw4/Controllers/HomeController.cs
+++ HW4/Hw4/Hw4/Controllers/HomeController.cs
@@ -23,11 +23,11 @@ namespace Hw4.Controllers
                 ViewBag.caseHeader = "Your input:";
 
                 one = one.Equals("") ? one : one.First().ToString() + one.Substring(1);
-                ViewBag.additionMessage = "Your names are magically lowercase! "  + one + " " + two;
+                ViewBag.additionMessage = "Your names are magically lowercase! " + one + " " + two;
             }
             return View();
         }
-        
+
         public ActionResult PostPage()
         {
             return View();
@@ -39,7 +39,7 @@ namespace Hw4.Controllers
             int user = form["username"].Length;
             int pass = form["password"].Length;
 
-            string[] totalLength = { "Total Length: " + (user + pass)};
+            string[] totalLength = { "Total Length: " + (user + pass) };
 
             ViewBag.mess1 = "Username Length: " + user;
             ViewBag.mess2 = "Password Length: " + pass;
@@ -47,5 +47,45 @@ namespace Hw4.Controllers
             return View();
         }
 
+        public ActionResult LoanCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoanCalc(int? startingBalance, double? interest, int? time)
+        {
+
+            double i = interest ?? -1.0;
+            int t = time ?? -1;
+            int s = startingBalance ?? -1;
+
+            if ((i == -1.0) | (t == -1) | (s == -1))
+            {
+                ViewBag.Error = "Please complete the form before submitting";
+                return View();
+            }
+
+            double monthlyRate = CalcMonth(s, i, t);
+
+            ViewBag.Sum = Math.Round(monthlyRate * (t * 12), 2);
+            ViewBag.Monthly = Math.Round(monthlyRate, 2);
+
+            return View();
+        }
+
+        public double CalcMonth(int s, double i, int t)
+        {
+            {
+                double monthInt;
+                monthInt = (i / 100) / 12;
+                double payment;
+                payment = t * 12;
+
+
+                return s * (monthInt / (1 - Math.Pow((1 + monthInt), -payment)));
+            }
+
+        }
     }
 }
