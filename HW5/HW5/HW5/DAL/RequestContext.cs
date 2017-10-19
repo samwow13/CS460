a81087b1e6f9f7204f779155189c4d90fca1f4bc
Sam Wetzel <samwow13@yahoo.com>
2017-10-19 16:58:25 -0700

    added DAL/requestcontext and model class

---
+++ HW5/HW5/HW5/DAL/RequestContext.cs
@@ -0,0 +1,14 @@
+﻿using HW5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace HW5.DAL
+{
+    public class RequestContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+    }
+}
---
+++ HW5/HW5/HW5/Models/Request.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HW5.Models
+{
+    public class Request
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "ODL#: "), Required]
+        public int ODL { get; set; }
+
+        [Display(Name = "Date of Birth: "), Required]
+        public string DoB { get; set; }
+
+        [Display(Name = "Full Name: "), Required]
+        public string FullName { get; set; }
+
+        [Display(Name = "Street Address: ")]
+        public string StreetAddress { get; set; }
+
+        [Display(Name = "City: ")]
+        public string City { get; set; }
+
+        [Display(Name = "State: ")]
+        public string States { get; set; }
+
+        [Display(Name = "Zipcode: ")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "County: ")]
+        public string County { get; set; }
+
+    }
+}
