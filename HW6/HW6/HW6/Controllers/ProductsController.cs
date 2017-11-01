5d610c2cfdb20cdc6e97573fe00275627a7e8a01
Sam Wetzel <samwow13@yahoo.com>
2017-10-31 20:16:30 -0700

    added controllers and the starter view

---
+++ HW6/HW6/HW6/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using HW6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW6.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AdventureWorksContext db = new AdventureWorksContext();
+
+        public ActionResult Index(int? id)
+        {
+            var cat = db.ProductCategories;
+            //determine if a ProductCategory was selected
+            if (id != null && db.ProductCategories.Find(id) != null)
+            {
+                ViewBag.ID = id;
+            }
+
+            return View(cat);
+        }
+    }
+}
