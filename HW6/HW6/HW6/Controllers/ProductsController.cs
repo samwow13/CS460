59a3c3199b6e84034c093319d057b3cf6f66d72b
Sam Wetzel <samwow13@yahoo.com>
2017-11-01 18:25:25 -0700

    added products method to controller and view

--- HW6/HW6/HW6/Controllers/ProductsController.cs
+++ HW6/HW6/HW6/Controllers/ProductsController.cs
@@ -13,14 +13,33 @@ namespace HW6.Controllers
 
         public ActionResult Index(int? id)
         {
-            var cat = db.ProductCategories;
-            //determine if a ProductCategory was selected
+            var category = db.ProductCategories;
             if (id != null && db.ProductCategories.Find(id) != null)
             {
                 ViewBag.ID = id;
             }
 
-            return View(cat);
+            return View(category);
+        }
+
+        public ActionResult Products(int? id, int? page = 1)
+        {
+            if (id == null || db.ProductSubcategories.Find(id) == null)
+                return RedirectToAction("Index");
+
+
+            var products = db.ProductSubcategories.Find(id).Products.ToList();
+            int pageSize = 9; 
+            double pagesNum = Math.Ceiling((double)products.Count / pageSize); 
+
+            int pageNumber = page ?? 0;
+            if (page < 1 || page > pagesNum)
+                return HttpNotFound(); 
+
+            ViewBag.NumberOfPages = pagesNum;
+            
+            var productsPaged = products.Skip(pageSize * (pageNumber - 1)).Take(pageSize);
+            return View(productsPaged);
         }
     }
 }
