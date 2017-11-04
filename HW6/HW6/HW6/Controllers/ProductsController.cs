948e114ee7116cef6a91f9086ab9869f13eee05c
Sam Wetzel <samwow13@yahoo.com>
2017-11-03 20:00:50 -0700

    added product class and view

--- HW6/HW6/HW6/Controllers/ProductsController.cs
+++ HW6/HW6/HW6/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,7 +28,6 @@ namespace HW6.Controllers
             if (id == null || db.ProductSubcategories.Find(id) == null)
                 return RedirectToAction("Index");
 
-
             var products = db.ProductSubcategories.Find(id).Products.ToList();
             int pageSize = 9; 
             double pagesNum = Math.Ceiling((double)products.Count / pageSize); 
@@ -40,6 +40,31 @@ namespace HW6.Controllers
             
             var productsPaged = products.Skip(pageSize * (pageNumber - 1)).Take(pageSize);
             return View(productsPaged);
+        }
+
+        public ActionResult Product(int? id)
+        {
+            if (id == null) // product id wasn't indicated
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var product = db.Products.Find(id);
+
+            if (product == null) // product doesn't exist
+                return HttpNotFound();
+
+            // get the number of product reviews, and if not 0, calculate the overall product rating
+            ViewBag.NumOfReviews = product.ProductReviews.Count;
+            ViewBag.Rating = ViewBag.NumOfReviews == 0 ? "N/A" : Math.Round(product.ProductReviews.Average(p => p.Rating), 2).ToString() + "/5";
+
+            // get the unit of measure for the product size if it has one
+            var sizeUnit = product.SizeUnitMeasureCode;
+            ViewBag.SizeUnit = sizeUnit == null ? "N/A" : product.SizeUnitMeasureCode.ToLower();
+
+            // get the unit of measure for the product weight if it has one
+            var weightUnit = product.WeightUnitMeasureCode;
+            ViewBag.WeightUnit = weightUnit == null ? "N/A" : product.WeightUnitMeasureCode.ToLower();
+
+            return View(product);
         }
     }
 }
